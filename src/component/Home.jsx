371175455d469240391d795b63946f77d3b2305e
sheeshagar-iqbal <iqbalkhan234786@gmail.com>
2026-06-08 11:49:29 +0530

    home deco

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,39 +1,40 @@
 
 import Card from 'react-bootstrap/Card';
+import banner from '../image/banner-img.jpg'
 
 const Home = () => {
   return (
     <>
-    <div className='hcontainer'>
+    <div className='hcontainer '>
         <div className='hcontain'>
-           
-
-        
-
-        </div>
-        
-        <Card style={{ width: '18rem' }}>
-        <Card.Body>
-        <Card.Subtitle className="mb-2 text-muted"> <div className="hbadge">
+             <Card style={{ width: '28rem' }}>
+        <Card.Body className='cardbody'>
+        <Card.Subtitle className="mb-2 text-muted "> <div className="hbadge">
                 <div className='hstar'><i class="fa-solid fa-star"></i></div>
                 <span>#1 Rated Fast Food Restaurant in New York</span>
             </div></Card.Subtitle>
         <Card.Title>
             <div className="htitle">
-                <h1>Delicious Fast Food <br />  for Every Moment</h1>
+                <h1>Delicious <span>Fast Food</span> <br />  for Every Moment</h1>
             </div>
         </Card.Title>
         
         <Card.Text className='htext'>
           Experience bold flavors crafted from premium ingredients. From crispy burgers to gourmet pizzas - every bite is an adventure worth savoring.
         </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Link href="#" className='btn'><i className="fas fa-utensils">
+            </i>Explore Menu</Card.Link>
+        <Card.Link href="#" className='btnwatch'><i class="fa-solid fa-play"></i>Watch Our Story</Card.Link>
       </Card.Body>
     </Card>
 
+        
+        </div>
+        
+      
+
         <div className='himg'>
-            <img src="https://themewagon.github.io/sarab/img/banner-img.jpg" alt="" />
+            <img src={banner} alt="" />
         </div>
 
     </div>
